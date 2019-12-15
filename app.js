e0bb8b94cd7364e23167d88fcfbea9cf1358c78d
Pendi Panel <pendipanel@gmail.com>
2019-12-16 00:40:06 +0600

    Query Selector

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const titles = document.getElementsByClassName('title');
+const wmf = 
+document.querySelector('#book-list li:nth-child(2) .name');
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
+// console.log(wmf);
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
+var books = document.querySelector('#book-list li .name');
+// console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+
+// console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
+  
+})
